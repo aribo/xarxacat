87ef0e8a5b26d22daf85130fcff5c8ff728a964a
Alejandro Ribó <alejandro@aribo.eu>
2014-03-26 20:05:51 +0000

    Remove model Pais from admin

--- xarxacat_site/dashboard/admin.py
+++ xarxacat_site/dashboard/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
-from dashboard.models import Pais
 from exteriors.models import Membre
 from xarxacat_site.models import XarxacatUsers 
 
 # Register your models here.
 
 admin.site.register(Membre)
-admin.site.register(Pais)
