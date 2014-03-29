e77ff0bbcbfda8b81ff97905e5c6d361cff34a8d
Alejandro Ribó <alejandro@aribo.eu>
2014-03-29 05:59:32 +0000

    First set up admin

--- xarxacat_site/dashboard/admin.py
+++ xarxacat_site/dashboard/admin.py
@@ -1,13 +1,37 @@
 from django.contrib import admin
 
-from exteriors.models import Membre
+from actions import export_as_csv_action
+
+from exteriors.models import Membre, Ae
 from xarxacat_site.models import XarxacatUsers 
 from cities_light.models import City
-
-from smart_selects.db_fields import ChainedForeignKey 
+ 
 
 # Register your models here.
 
-admin.site.register(Membre)
+#class MembreInline(admin.TabularInline):
+#    	model = Membre
+#    	extra = 5
+#    	fields = ('nom','cognoms','email')
+
+class MembreAdmin (admin.ModelAdmin):
+		list_filter = ['tipus','registre_anc','ae','estat']
+		list_display = ('nom','cognoms','ae','tipus','pagament')
+		search_fields = ['nom','cognoms','ae__nom_ca',]
+		readonly_fields = ('dataactualitzacio',)
+		actions = [export_as_csv_action("CSV Export", fields=['nom','cognoms','email','ae'])]
+		
+		
+
+class AeAdmin (admin.ModelAdmin):
+		list_filter = ['constitucio','activitat']
+		# = ("constitucio",)
+#		inlines = [MembreInline,]
+
+
+admin.site.register(Membre, MembreAdmin)
+admin.site.register(Ae,AeAdmin)
+
+
 
 
--- xarxacat_site/exteriors/models.py
+++ xarxacat_site/exteriors/models.py
@@ -7,25 +7,32 @@ from cities_light.models import Country, City
 from smart_selects.db_fields import ChainedForeignKey 
 
 
+# Choices
+
+AE_TIPUS = (
+	(1, 'Nacional'),
+	(2, 'Local'),
+	(3, 'Regional'),
+	)
+CONSTITUCIO = (
+	(11, 'Sí'),
+	(00, 'No'),
+	(01, 'En procés'),
+	)
+
+STATUS_PAGAMENT = (
+	(11, 'Corrent'),
+	(00, 'Pendent'),
+	)
+
+BINARI = (
+	(11, 'Sí'),
+	(00, 'No'),
+	)
+	
+
 # Create your models here.
 
-class Ae(models.Model):
-    tipus = models.CharField(max_length=255)
-    area_ca = models.CharField(max_length=100)
-    area_en = models.CharField(max_length=100)
-    area_lang = models.CharField(max_length=100)
-    country = models.IntegerField(blank=True, null=True)
-    constitution = models.IntegerField()
-    active = models.IntegerField()
-    constitution_date = models.DateField(blank=True, null=True)
-    email = models.CharField(max_length=100)
-    facebook = models.CharField(max_length=255, blank=True)
-    twitter = models.CharField(max_length=100, blank=True)
-    web = models.CharField(max_length=255, blank=True)
-    def __unicode__(self): 
-    	return self.area_ca
-	class Meta:
-       		ordering = ['area_ca']
     	
 class CarrecAe(models.Model):
     nom = models.CharField(max_length=255)
@@ -34,6 +41,7 @@ class CarrecAe(models.Model):
     	return self.nom 
 	class Meta:
         	ordering = ['id'] 
+        	
         	
 class CarrecCaec(models.Model):
     nom = models.CharField(max_length=255)
@@ -51,20 +59,32 @@ class MembreTipus(models.Model):
 	class Meta:
         	ordering = ['id'] 
 
-class RegistreAnc(models.Model):
-	label = models.CharField(max_length=4)
+class Ae(models.Model):
+	# tipus = models.IntegerField(choices=AE_TIPUS)
+	dataentrada = models.DateTimeField(#auto_now_add=True,
+	verbose_name="Data entrada")
+	nom = models.CharField(max_length=100, verbose_name="Nom", help_text="En la llengua originària del país")
+	nom_ca = models.CharField(max_length=100, verbose_name="Nom en català")
+	nom_en = models.CharField(max_length=100, verbose_name="Nom en anglès")
+	estat =  models.ForeignKey(Country, verbose_name="Estat")
+	constitucio = models.IntegerField(choices=CONSTITUCIO, verbose_name="Constitució")
+	data_constitucio = models.DateField(blank=True, null=True, verbose_name="Data constitució")
+	activitat = models.IntegerField(choices=BINARI, verbose_name="Activitat", help_text="Indicar si l'AE està activa")
+	email = models.CharField(max_length=100)
+	facebook = models.CharField(max_length=255, blank=True)
+	twitter = models.CharField(max_length=100, blank=True)
+	web = models.CharField(max_length=255, blank=True)
 	def __unicode__(self):
-		return self.label
-		
-class Pagament(models.Model):
-	status = models.CharField(max_length=10)
-	def __unicode__(self):
-		return self.status
-	
+		return self.nom_ca
+	class Meta:
+		ordering = ['nom_ca']
+		verbose_name = "Assemblea Exterior"
+		verbose_name_plural = "Assemblees Exteriors"
 
 
 class Membre(models.Model):
-    dataentrada = models.DateTimeField(verbose_name="Data entrada")
+    dataentrada = models.DateTimeField(auto_now_add=True, verbose_name="Data entrada")
+    dataactualitzacio = models.DateTimeField(auto_now=True, verbose_name="Darrera actualització")
     nom = models.CharField(max_length=255)
     cognoms = models.CharField(max_length=255)
     dni = models.CharField(max_length=20, blank=True, verbose_name="DNI")
@@ -74,18 +94,18 @@ class Membre(models.Model):
     telefon = models.CharField(max_length=20, blank=True, verbose_name="Telèfon")
     ae = models.ForeignKey(Ae, verbose_name="Assemblea Exterior")
     codipostal = models.CharField(max_length=255, blank=True, verbose_name="Codi postal")
-    estat = models.ForeignKey(Country)
+    estat = models.ForeignKey(Country, verbose_name="Estat")
     poblacio_ext = ChainedForeignKey(City,
-    	chained_field="pais",
+    	chained_field="estat",
     	chained_model_field="country",
     	show_all=False,
     	auto_choose=True,
     	blank=True, null=True, related_name = 'poblacio_ext_set', verbose_name="Població a l'estranger", help_text="Població a l'estranger on el membre resideix normalment")
     poblacio_cat = models.ForeignKey(City, limit_choices_to={'region_id': 921},
     	blank=True, null=True, related_name = 'poblacio_cat_set', verbose_name="Població a Catalunya", help_text="Població a Catalunya d'on el membre prové")
-    tipus = models.ForeignKey(MembreTipus, help_text="Tipus de membre")
-    registre_anc = models.ForeignKey(RegistreAnc, verbose_name="Registre ANC")
-    pagament = models.ForeignKey(Pagament, default="Corrent")
+    tipus = models.ForeignKey(MembreTipus, verbose_name="Tipus", help_text="Tipus de membre")
+    registre_anc = models.IntegerField(choices=BINARI, verbose_name="Registre ANC")
+    pagament = models.IntegerField(choices=STATUS_PAGAMENT, default="Corrent", verbose_name="Pagament de quotes")
     dataregistre_anc = models.DateField(blank=True, null=True, verbose_name="Data registre a l'ANC")
     numero_anc = models.CharField(max_length=255, blank=True, verbose_name="Número soci ANC")
     sectorial_anc = models.CharField(max_length=255, blank=True, verbose_name="Sectorial")
@@ -95,6 +115,7 @@ class Membre(models.Model):
     carrec_caec = models.ForeignKey(CarrecCaec, blank=True, null=True, verbose_name="Càrrec al CAEC",  help_text="Si el membre té un càrrec al Consell, si no deixar buit")
     def __unicode__(self): 
         return u'%s %s' % (self.nom, self.cognoms)
+    search_fields = ['nom,cognoms']
    
 
 
