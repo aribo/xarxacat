941a5a07e44c1199263974adb025758e691b3877
Alejandro Ribó <alejandro@aribo.eu>
2014-03-31 16:35:11 +0100

    New premsa app.

---
+++ xarxacat_site/premsa/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+# Create your models here.
+
+'''
+
+class Mitja(models.Model):
+	nom = model.Charfield(max_length=50)
+	pais
+	tipus
+	idioma
+	email = model.Charfield(max_length=50)
+	nota
+	data_entrada
+	data_actualitzacio
+	
+class Agencia(models.Model):
+	nom = model.Charfield(max_length=50)
+	pais
+	tipus
+	idioma
+	email
+	nota = model.Charfield(max_length=50)
+	data_entrada
+	data_actualitzacio
+
+class Periodista(models.Model):
+	nom = model.Charfield(max_length=50)
+	cognoms = = model.Charfield(max_length=100)
+	email = model.Charfield(max_length=50)
+	tipus
+	pais_origen
+	idioma
+	telefon = model.Charfield(max_length=50)
+	twitter = model.Charfield(max_length=50)
+	skype = model.Charfield(max_length=50)
+	adreça
+	codipostal = model.Charfield(max_length=50)
+	pais
+	ciutat
+	agencia
+	mitja
+	contacte
+	nota
+	data_entrada
+	data_actualitzacio
+	
+class Contacte(models.Model):
+	titol = model.Charfield(max_length=50)
+	descripcio
+	autor
+	resultat
+	data_entrada
+	data_actualitzacio
+
+class Notes(models.Model):
+	nota
+	autor
+	data_entrada
+	data_actualitzacio
