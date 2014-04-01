0af7cad843267d99438431cae9b2e526af7032ee
Alejandro Ribó <alejandro@aribo.eu>
2014-04-01 02:48:34 +0100

    Add a monitor of changes for wsgi

--- xarxacat_site/xarxacat_site/wsgi.py
+++ xarxacat_site/xarxacat_site/wsgi.py
@@ -12,3 +12,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xarxacat_site.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+import xarxacat_site.monitor
+xarxacat_site.monitor.start(interval=1.0)
