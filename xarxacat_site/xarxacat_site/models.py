0d76f92a31f9b57cd48515085679bad38582d9d4
Alejandro Ribó <alejandro@aribo.eu>
2014-04-04 01:38:52 +0100

    Install new model for languages by country

--- xarxacat_site/xarxacat_site/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class XarxacatUsers(models.Model):
-    name = models.CharField(max_length=255)
-    class Meta:
-        managed = True
-        db_table = 'xarxacat_users'
-        ordering = ['name'] 
